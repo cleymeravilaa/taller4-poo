560e1b86b082a2d84fbe1b936a748297830c4829
Cleymer Avila <cleymer.avilapushaina@unicolombo.edu.co>
2024-11-04 21:12:00 -0500

    Ejercicio 4 POO update.

---
+++ BankAccount.java
@@ -0,0 +1,55 @@
+public class BankAccount {
+    
+    // No es correcto declarar atributos y datos criticos y delicados como protected
+    // protected double balance;
+    // protected String accountNumber;
+    // protected String owner;
+    // protected String password;
+
+    // Sería más apropiado declarar atributos y datos como private
+    private double balance; 
+    private String accountNumber;
+    private String owner;
+    private String password;
+
+    public BankAccount(double balance){
+        // Verificar que el saldo no sea negativo
+        if (balance<0) {
+            this.balance = 0;
+        }else {
+            this.balance = balance;
+        }
+    }
+
+    public void deposit(double amount){
+        // Encapsular la variable saldo de manera adecuada para que no pueda ser facilmente manipulada
+        if (amount<0) {
+            System.out.println("No se puede depositar un saldo negativo");
+        } else {
+            balance += amount;
+        }
+    }
+
+    public void withdraw(double amount){
+        if (amount>0) {
+            System.out.println("No se puede retirar un saldo positivo");
+        } else {
+            balance -= amount;
+        }
+    }
+
+    // La mejor manera de encapsular datos sensibles es creando getters y setters
+    // Siempre y sea necesario
+    public double getBalance(){
+        return balance;
+    }
+
+    public String getAccountNumber(){
+        return accountNumber;
+    }
+
+    public String getOwner(){
+        return owner;
+    }
+
+}
